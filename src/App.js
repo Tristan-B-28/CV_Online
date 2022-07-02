d1beebbe3ba9064d351bb2210ffbd7df5ba7ca88
Tristan <tristanbeau33@gmail.com>
2022-07-02 05:49:28 +0200

    //[ADD] sidebar with navigation but without routes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Knowledges from './pages/Knowledges';
+import NotFound from './pages/NotFound';
+import Portfolio from './pages/Portfolio';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/competences" element={<Knowledges />} />
+          <Route path="/notFound" element={<NotFound />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+};
+
+export default App;
